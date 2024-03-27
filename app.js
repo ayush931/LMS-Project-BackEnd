21c9751d5831197c1e02a2bf6746615fd25ac3fe
ayush931 <159576275+ayush931@users.noreply.github.com>
2024-03-27 20:44:24 +0530

    Register flow of the backEnd completed.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import { config } from 'dotenv'
 import morgan from 'morgan'
+import userRouters from './routes/user.routes.js'
+import errorMiddleware from './middlewares/error.middleware.js'
 config()
 
 const app = express()
@@ -22,10 +24,12 @@ app.use('/ping', function(req, res){
     res.send('/pong')
 })
 
-// routes of 3 modules
+app.use('/api/v1/user', userRouters)
 
 app.all('*', (req, res) => {
     res.status(404).send('OOPS!! 404 page not found')
 })
+
+app.use(errorMiddleware)
 
 export default app
